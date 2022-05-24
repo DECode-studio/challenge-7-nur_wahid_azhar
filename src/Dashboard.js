81296b9afc88a61c56950c51bc4f071d5bfd965a
DECode.studio <decode.std@gmail.com>
2022-05-24 08:54:55 +0700

    clear dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -88,7 +88,7 @@ function Content() {
         <div>
             <div className="container p-md-5 d-flex ctx-resp">
                 <div className="p-5">
-                    <img className="img-responsive" id="img_service" src={require('../images/img_service.png')} />
+                    <img className="img-responsive" id="img_service" src={require('./images/img_service.png')} />
                 </div>
                 <div className="align-self-center">
                     <h3>
@@ -100,31 +100,31 @@ function Content() {
                         meeting, dll.
                     </div>
                     <div className="d-flex pb-3">
-                        <img src="/images/img_checklist.png" height={24} width={24} />
+                        <img src={require("./images/img_checklist.png")} height={24} width={24} />
                         <div className="text ps-3">
                             Sewa Mobil Dengan Supir di Bali 12 Jam
                         </div>
                     </div>
                     <div className="d-flex pb-3">
-                        <img src="/images/img_checklist.png" height={24} width={24} />
+                        <img src={require("./images/img_checklist.png")} height={24} width={24} />
                         <div className="text ps-3">
                             Sewa Mobil Lepas Kunci di Bali 24 Jam
                         </div>
                     </div>
                     <div className="d-flex pb-3">
-                        <img src="/images/img_checklist.png" height={24} width={24} />
+                        <img src={require("./images/img_checklist.png")} height={24} width={24} />
                         <div className="text ps-3">
                             Sewa Mobil Jangka Panjang Bulanan
                         </div>
                     </div>
                     <div className="d-flex pb-3">
-                        <img src="/images/img_checklist.png" height={24} width={24} />
+                        <img src={require("./images/img_checklist.png")} height={24} width={24} />
                         <div className="text ps-3">
                             Gratis Antar - Jemput Mobil di Bandara
                         </div>
                     </div>
                     <div className="d-flex pb-3">
-                        <img src="/images/img_checklist.png" height={24} width={24} />
+                        <img src={require("./images/img_checklist.png")} height={24} width={24} />
                         <div className="text ps-3">
                             Layanan Airport Transfer / Drop In Out
                         </div>
@@ -144,7 +144,7 @@ function Content() {
                                 <div className="card" style={{ width: '16rem' }}>
                                     <div className="card-body">
                                         <div className="pb-3">
-                                            <img src="/images/icon_complete.png" height={32} width={32} />
+                                            <img src={require("./images/icon_complete.png")} height={32} width={32} />
                                         </div>
                                         <h5 className="card-title">Mobil Lengkap</h5>
                                         <p className="card-text">Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan
@@ -157,7 +157,7 @@ function Content() {
                                 <div className="card" style={{ width: '16rem' }}>
                                     <div className="card-body">
                                         <div className="pb-3">
-                                            <img src="/images/icon_price.png" height={32} width={32} />
+                                            <img src={require("./images/icon_price.png")} height={32} width={32} />
                                         </div>
                                         <h5 className="card-title">Harga Murah</h5>
                                         <p className="card-text">Harga murah dan bersaing, bisa bandingkan harga kami dengan rental
@@ -170,7 +170,7 @@ function Content() {
                                 <div className="card" style={{ width: '16rem' }}>
                                     <div className="card-body">
                                         <div className="pb-3">
-                                            <img src="/images/icon_24hrs.png" height={32} width={32} />
+                                            <img src={require("./images/icon_24hrs.png")} height={32} width={32} />
                                         </div>
                                         <h5 className="card-title">Layanan 24 Jam</h5>
                                         <p className="card-text">Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga
@@ -184,7 +184,7 @@ function Content() {
                                 <div className="card" style={{ width: '16rem' }}>
                                     <div className="card-body">
                                         <div className="pb-3">
-                                            <img src="/images/icon_professional.png" height={32} width={32} />
+                                            <img src={require("./images/icon_professional.png")} height={32} width={32} />
                                         </div>
                                         <h5 className="card-title">Sopir Profesional</h5>
                                         <p className="card-text">Sopir yang profesional, berpengalaman, jujur, ramah dan selalu
@@ -201,12 +201,77 @@ function Content() {
         </div>
     )
 }
+
+function Footer() {
+    return (
+        <div className="bg-light mt-5">
+            <div className="container mt-xxl-5 p-5">
+                <div className="row">
+                    <div className="col">
+                        <p>
+                            Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000
+                        </p><p>
+                        </p><p>
+                            binarcarrental@gmail.com
+                        </p>
+                        <p>
+                            081-233-334-808
+                        </p>
+                    </div>
+                    <div className="col">
+                        <p style={{ fontWeight: 'bold' }}>
+                            Our services
+                        </p><p>
+                        </p><p style={{ fontWeight: 'bold' }}>
+                            Why Us
+                        </p>
+                        <p style={{ fontWeight: 'bold' }}>
+                            Testimonial
+                        </p>
+                        <p style={{ fontWeight: 'bold' }}>
+                            FAQ
+                        </p>
+                    </div>
+                    <div className="col">
+                        <p>Connect with us</p>
+                        <div className="d-flex">
+                            <div className="p-2">
+                                <img src={require("./images/icon_facebook.png")} width={32} height={32} />
+                            </div>
+                            <div className="p-2">
+                                <img src={require("./images/icon_instagram.png")} width={32} height={32} />
+                            </div>
+                            <div className="p-2">
+                                <img src={require("./images/icon_twitter.png")} width={32} height={32} />
+                            </div>
+                            <div className="p-2">
+                                <img src={require("./images/icon_mail.png")} width={32} height={32} />
+                            </div>
+                            <div className="p-2">
+                                <img src={require("./images/icon_twitch.png")} width={32} height={32} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <p>
+                            Copyright Binar 2022
+                        </p>
+                        <img src={require("./images/Rectangle_74.png")} width={100} height={34} />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
 function Dashboard() {
     return (
         <div>
             <Head />
             <ToolBar />
             <Header />
+            <Content />
+            <Footer />
         </div>
     )
 }
